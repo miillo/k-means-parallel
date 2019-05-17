f4f7a1fa8df2b480ae7b54fcddf2074ae19c9680
Miłosz Pluta <milosz.pluta2@gmail.com>
2019-05-17 21:32:12 +0200

    KMeans on dataframes without parallel executions

--- src/main/scala/com/kmeans/Executor.scala
+++ src/main/scala/com/kmeans/Executor.scala
@@ -1,9 +1,9 @@
 package com.kmeans
 
 import com.kmeans.utils.ApplicationProperties
-import org.apache.spark.sql.{Column, Row, SparkSession}
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{FloatType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.{Column, Row, SparkSession}
 
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
@@ -16,27 +16,16 @@ object Executor {
     val sourceDf = sparkSession.read.format("csv")
       .option("sep", ",")
       .option("header", value = true)
-      .load("DataSets/iris.data")
+      .load(properties.sourceDataPath)
     sourceDf.show(20, truncate = false)
 
-    //test area
-    //*************************
-    //    import sparkSession.implicits._
-    //    val df = Seq((1, "one"), (2, "two"), (3, "three")).toDF()
-    //    df.show(10)
-    //
-    //    val col = df.col("_2")
-    //    val dropped = df.drop("_2")
-    //    val dropclone = sparkSession.createDataFrame(dropped.rdd, dropped.schema)
-    //    dropclone.show(10)
-    //    println(col)
-    //
-    //    dropclone.withColumn("_2", col).show(10)
-    //*************************
+    //preparing source dfs
+    val validationDf = sourceDf
+      .select("class")
+      .withColumn("ID", monotonically_increasing_id())
+    validationDf.cache()
+    validationDf.count()
 
-    //save class column
-    val classCol = sourceDf.col("class")
-    //delete class column
     val withoutClass = sourceDf
       .drop("class")
 
@@ -62,101 +51,114 @@ object Executor {
       }
     }
 
-    /**
-      * by now transforming init centroids to df is suspended
-      */
-    //    // create centroids df schema
+    // create centroids df schema
     val fieldsList = new ListBuffer[StructField]
     for (i <- 0 until maxRow.length) {
       fieldsList += StructField("_c" + i, StringType, nullable = false)
     }
     fieldsList += StructField("clusterDecision", StringType, nullable = false)
-    //
-    //    // create centroids df
-    //    val mappedCentroids = centroidsInit.map(el => Row.fromSeq(el))
-    //    val centRdd = sparkSession.sparkContext.makeRDD(mappedCentroids)
-    //    val centroidsDf = sparkSession.createDataFrame(centRdd, StructType(fieldsList))
-    //    centroidsDf.show(20,false)
+    fieldsList += StructField("ID", LongType, nullable = false)
+
+    var pointsClustered = sparkSession
+      .createDataFrame(sparkSession.sparkContext.emptyRDD[Row], StructType(fieldsList))
 
     val sourceDataDfPrepared = withoutClass
       .withColumn("clusterDecision", lit(1))
+      .withColumn("ID", monotonically_increasing_id())
 
-    // for each point xi ..
-    var newClusters = sourceDataDfPrepared
-      .rdd
-      .map(el => {
-        var decision = ""
-        var result = Double.MaxValue
-        for (centroid <- centroidsInit) {
-          var distTmp = 0.0
-          // - 1 because last is reserved for 'clusterDecision'     //[5.1,3.5,1.4,0.2,1]
-          for (i <- 0 until el.length - 1) {
-//            println("cent " + centroid._1 + " val: " + centroid._2(i) + " | el val: " +  el.get(i).asInstanceOf[String].toFloat)
-            //compute euclidean distance
-            distTmp += scala.math.pow(centroid._2(i) - el.get(i).asInstanceOf[String].toFloat, 2)
+    for (_ <- 0 to properties.iterations) {
+      // for each point xi ..
+      var newClusters = sourceDataDfPrepared
+        .rdd
+        .map(el => {
+          var decision = ""
+          var result = Double.MaxValue
+          for (centroid <- centroidsInit) {
+            var distTmp = 0.0
+            // - 2 because last is reserved for 'clusterDecision' and ID     //[5.1,3.5,1.4,0.2,1]
+            for (i <- 0 until el.length - 2) {
+              //            println("cent " + centroid._1 + " val: " + centroid._2(i) + " | el val: " +  el.get(i).asInstanceOf[String].toFloat)
+              //compute euclidean distance
+              distTmp += scala.math.pow(centroid._2(i) - el.get(i).asInstanceOf[String].toFloat, 2)
+            }
+            val res = scala.math.sqrt(distTmp)
+            if (res < result) {
+              result = res
+              decision = centroid._1
+            }
           }
-          val res = scala.math.sqrt(distTmp)
-          if (res < result) {
-            result = res
-            decision = centroid._1
-          }
-        }
-        val clusterDecIndex = el.fieldIndex("clusterDecision")
-        val updatedRow = el
-          .toSeq
-          .updated(clusterDecIndex, decision)
-        Row.fromSeq(updatedRow)
-      })
+          val clusterDecIndex = el.fieldIndex("clusterDecision")
+          val updatedRow = el
+            .toSeq
+            .updated(clusterDecIndex, decision)
+          Row.fromSeq(updatedRow)
+        })
 
-    //preparing df for second step computations
-    var pointsClustered = sparkSession
-      .createDataFrame(newClusters, StructType(fieldsList))
+      //preparing df for second step computations
+      //cache for persisting this df for further computations
+      pointsClustered = sparkSession
+        .createDataFrame(newClusters, StructType(fieldsList))
+        .cache()
 
-    for (colName <- pointsClustered.columns) {
-      if (colName == "clusterDecision") {
-        pointsClustered = pointsClustered.withColumn(colName, col(colName).cast("String"))
-      } else {
-        pointsClustered = pointsClustered.withColumn(colName, col(colName).cast("float"))
-      }
-    }
-
-    println("XXXXXXXXXXXXXXXXXXX")
-    pointsClustered.filter($"clusterDecision" === "k0").show(5)
-    println(pointsClustered.filter($"clusterDecision" === "k0").count())
-    pointsClustered.filter($"clusterDecision" === "k1").show(5)
-    println(pointsClustered.filter($"clusterDecision" === "k1").count())
-    //TODO Tutaj przed petla count wypisuje np. 54 dla k2
-    pointsClustered.filter($"clusterDecision" === "k2").show(5)
-    println(pointsClustered.filter($"clusterDecision" === "k2").count())
-
-
-    //for each cluster j ..
-    println(centroidsInit)
-    for (centroid <- centroidsInit) {
-      println("Centroid type: " + centroid.getClass + " | centroid name type: " + centroid._1.getClass)
-      println("Centroid: " + centroid)
-      val centroidName = centroid._1
-      //TODO prawdopodbnie tutaj filtrowanie sie psuje, ale dlaczego? wartosc centroidName jest ok - probowalem tez z fun. 'like'
-      val filtered = pointsClustered.filter($"clusterDecision" === centroidName)
-      filtered.show(20)
-      //TODO natomiast tutaj wypisuje inna wartosc niz ta przed petla mimo ze to ta sama instancja?
-      println(filtered.count())
-      val filteredColumns = filtered.columns
-      for (i <- filtered.columns.indices) {
-        if (filteredColumns(i) != "clusterDecision") {
-          //compute mean
-          val computedMean = filtered
-            .select(mean(col(filteredColumns(i))))
-            .head()
-            .get(0)
-            .asInstanceOf[Double]
-            .toFloat
-
-          //TODO ta linijka powoduje takie zaklocenia
-          centroid._2.update(i, computedMean)
+      for (colName <- pointsClustered.columns) {
+        if (colName == "clusterDecision") {
+          pointsClustered = pointsClustered.withColumn(colName, col(colName).cast("String"))
+        } else if (colName == "ID") {
+          pointsClustered = pointsClustered.withColumn(colName, col(colName).cast("int"))
+        } else {
+          pointsClustered = pointsClustered.withColumn(colName, col(colName).cast("float"))
         }
       }
+
+      //      println("XXXXXXXXXXXXXXXXXXX")
+      //      pointsClustered.filter($"clusterDecision" === "k0").show(5)
+      //      println(pointsClustered.filter($"clusterDecision" === "k0").count())
+      //      pointsClustered.filter($"clusterDecision" === "k1").show(5)
+      //      println(pointsClustered.filter($"clusterDecision" === "k1").count())
+      //      //TODO Tutaj przed petla count wypisuje np. 54 dla k2
+      //      pointsClustered.filter($"clusterDecision" === "k2").show(5)
+      //      println(pointsClustered.filter($"clusterDecision" === "k2").count())
+
+      //for each cluster j ..
+      println(centroidsInit)
+      for (centroid <- centroidsInit) {
+        println("Centroid type: " + centroid.getClass + " | centroid name type: " + centroid._1.getClass)
+        println("Centroid: " + centroid)
+        val centroidName = centroid._1
+        //TODO prawdopodbnie tutaj filtrowanie sie psuje, ale dlaczego? wartosc centroidName jest ok - probowalem tez z fun. 'like'
+        val filtered = pointsClustered.filter($"clusterDecision" === centroidName)
+        filtered.show(20)
+        //TODO natomiast tutaj wypisuje inna wartosc niz ta przed petla mimo ze to ta sama instancja?
+        println(filtered.count())
+        val filteredColumns = filtered.columns
+        for (i <- filtered.columns.indices) {
+          if (filteredColumns(i) != "clusterDecision" && filteredColumns(i) != "ID") {
+            //compute mean
+            val computedMean = filtered
+              .select(mean(col(filteredColumns(i))))
+              .head()
+              .get(0)
+              .asInstanceOf[Double]
+              .toFloat
+
+            //          if (!meanValues.contains(centroidName)) {
+            //            meanValues += (centroidName -> ListBuffer(computedMean))
+            //          } else {
+            //            meanValues(centroidName) += computedMean
+            //          }
+
+            //TODO ta linijka powoduje takie zaklocenia
+            centroid._2.update(i, computedMean)
+          }
+        }
+      }
+
+      //    println(meanValues)
+      println(centroidsInit)
     }
-    println(centroidsInit)
+
+    pointsClustered
+      .join(validationDf, pointsClustered.col("ID") === validationDf.col("ID"))
+      .show(150)
   }
 }
--- src/main/scala/com/kmeans/utils/ApplicationProperties.scala
+++ src/main/scala/com/kmeans/utils/ApplicationProperties.scala
@@ -7,6 +7,7 @@ import com.typesafe.config.{Config, ConfigFactory}
 case class ApplicationProperties(cliObject: CliObject) {
   private val config: Config = ConfigFactory.parseFile(new File(cliObject.configPath))
 
+  val iterations: Int = config.getInt("params.iterations")
   val kParam: Int = config.getInt("params.k")
   val sourceDataPath: String = config.getString("source-data.path")
 }
