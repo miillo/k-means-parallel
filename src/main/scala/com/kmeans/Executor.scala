6507746e3c73200d495f50e87e84b9dd7163739e
Miłosz Pluta <milosz.pluta2@gmail.com>
2019-05-17 18:51:35 +0200

    Test 1

--- src/main/scala/com/kmeans/Executor.scala
+++ src/main/scala/com/kmeans/Executor.scala
@@ -10,6 +10,7 @@ import scala.util.Random
 
 object Executor {
   def execute(sparkSession: SparkSession, properties: ApplicationProperties): Unit = {
+    import sparkSession.implicits._
 
     //read source data
     val sourceDf = sparkSession.read.format("csv")
@@ -20,17 +21,17 @@ object Executor {
 
     //test area
     //*************************
-//    import sparkSession.implicits._
-//    val df = Seq((1, "one"), (2, "two"), (3, "three")).toDF()
-//    df.show(10)
-//
-//    val col = df.col("_2")
-//    val dropped = df.drop("_2")
-//    val dropclone = sparkSession.createDataFrame(dropped.rdd, dropped.schema)
-//    dropclone.show(10)
-//    println(col)
-//
-//    dropclone.withColumn("_2", col).show(10)
+    //    import sparkSession.implicits._
+    //    val df = Seq((1, "one"), (2, "two"), (3, "three")).toDF()
+    //    df.show(10)
+    //
+    //    val col = df.col("_2")
+    //    val dropped = df.drop("_2")
+    //    val dropclone = sparkSession.createDataFrame(dropped.rdd, dropped.schema)
+    //    dropclone.show(10)
+    //    println(col)
+    //
+    //    dropclone.withColumn("_2", col).show(10)
     //*************************
 
     //save class column
@@ -50,7 +51,6 @@ object Executor {
     //draw initial values for centroids
     for (i <- 0 until maxRow.length) {
       for (j <- 0 until properties.kParam) {
-        //        println("i: " + i + " @ j: " + j)
         val start = minRow.get(i).asInstanceOf[String].toFloat
         val end = maxRow.get(i).asInstanceOf[String].toFloat
         val draw = start + (end - start) * Random.nextFloat()
@@ -89,8 +89,9 @@ object Executor {
         var result = Double.MaxValue
         for (centroid <- centroidsInit) {
           var distTmp = 0.0
-          // - 1 because last is reserved for 'clusterDecision'
+          // - 1 because last is reserved for 'clusterDecision'     //[5.1,3.5,1.4,0.2,1]
           for (i <- 0 until el.length - 1) {
+//            println("cent " + centroid._1 + " val: " + centroid._2(i) + " | el val: " +  el.get(i).asInstanceOf[String].toFloat)
             //compute euclidean distance
             distTmp += scala.math.pow(centroid._2(i) - el.get(i).asInstanceOf[String].toFloat, 2)
           }
@@ -107,39 +108,55 @@ object Executor {
         Row.fromSeq(updatedRow)
       })
 
-    var testDf = sparkSession
+    //preparing df for second step computations
+    var pointsClustered = sparkSession
       .createDataFrame(newClusters, StructType(fieldsList))
 
-    testDf.show(10)
-    testDf.printSchema()
-
-    for (colName <- testDf.columns) {
+    for (colName <- pointsClustered.columns) {
       if (colName == "clusterDecision") {
-        println("HIHI")
-        testDf = testDf.withColumn(colName, col(colName).cast("String"))
+        pointsClustered = pointsClustered.withColumn(colName, col(colName).cast("String"))
       } else {
-        testDf = testDf.withColumn(colName, col(colName).cast("float"))
+        pointsClustered = pointsClustered.withColumn(colName, col(colName).cast("float"))
       }
     }
 
-    testDf.show(10)
-    testDf.printSchema()
-    //
-//    println("??????????")
-//    val decCol = testDf.col("clusterDecision")
-//    //is there event antyhing ?!
-//    println(decCol)
-//    val omg = testDf.select(testDf.columns.init.map(c => col(c).cast(FloatType)): _*)
-//    val omgomg = omg.withColumn("clusterDecision", decCol)
-//
-//    omgomg.printSchema()
-//    omgomg.show(20)
-//
-//    for (centroid <- centroidsInit) {
-//      val centroidName = centroid._1
-//
-//    }
-//    println("WOBO2")
-//    newClusters.take(20).foreach(println)
+    println("XXXXXXXXXXXXXXXXXXX")
+    pointsClustered.filter($"clusterDecision" === "k0").show(5)
+    println(pointsClustered.filter($"clusterDecision" === "k0").count())
+    pointsClustered.filter($"clusterDecision" === "k1").show(5)
+    println(pointsClustered.filter($"clusterDecision" === "k1").count())
+    //TODO Tutaj przed petla count wypisuje np. 54 dla k2
+    pointsClustered.filter($"clusterDecision" === "k2").show(5)
+    println(pointsClustered.filter($"clusterDecision" === "k2").count())
+
+
+    //for each cluster j ..
+    println(centroidsInit)
+    for (centroid <- centroidsInit) {
+      println("Centroid type: " + centroid.getClass + " | centroid name type: " + centroid._1.getClass)
+      println("Centroid: " + centroid)
+      val centroidName = centroid._1
+      //TODO prawdopodbnie tutaj filtrowanie sie psuje, ale dlaczego? wartosc centroidName jest ok - probowalem tez z fun. 'like'
+      val filtered = pointsClustered.filter($"clusterDecision" === centroidName)
+      filtered.show(20)
+      //TODO natomiast tutaj wypisuje inna wartosc niz ta przed petla mimo ze to ta sama instancja?
+      println(filtered.count())
+      val filteredColumns = filtered.columns
+      for (i <- filtered.columns.indices) {
+        if (filteredColumns(i) != "clusterDecision") {
+          //compute mean
+          val computedMean = filtered
+            .select(mean(col(filteredColumns(i))))
+            .head()
+            .get(0)
+            .asInstanceOf[Double]
+            .toFloat
+
+          //TODO ta linijka powoduje takie zaklocenia
+          centroid._2.update(i, computedMean)
+        }
+      }
+    }
+    println(centroidsInit)
   }
 }
